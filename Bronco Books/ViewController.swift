cfc07866b7a13476db84f750030b27f217ff2eda
Vineet Joshi <vineet0713@gmail.com>
2019-01-24 13:50:48 -0800

    Initial Commit

---
+++ Bronco Books/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bronco Books
+//
+//  Created by Vineet Joshi on 1/24/19.
+//  Copyright © 2019 Vineet Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bronco BooksTests/Bronco_BooksTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bronco_BooksTests.swift
+//  Bronco BooksTests
+//
+//  Created by Vineet Joshi on 1/24/19.
+//  Copyright © 2019 Vineet Joshi. All rights reserved.
+//
+
+import XCTest
+@testable import Bronco_Books
+
+class Bronco_BooksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
